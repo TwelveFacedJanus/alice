345a96b80ad2f6da106c5f1fdf2ed079a5919160
Даниил Ермолаев <ermolaevd@akber-soft.ru>
2026-04-21 18:19:07 +0500

    Starting separate logic to database manager

--- src/main.rs
+++ src/main.rs
@@ -206,7 +206,7 @@ impl Database {
         }
     }
 
-    pub fn process_command(&mut self, input: &str) -> io::Result<()> {
+    pub fn process_command(&mut self, input: &str) -> io::Result<bool> {
         let command_pairs = CommandsParser::parse(Rule::command, input)
             .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e.to_string()))?;
 
@@ -220,6 +220,7 @@ impl Database {
                     let table_name = inner.into_inner().nth(0).unwrap().as_str();
                     *self += Table::new(table_name, EngineType::RowEngine);
                     println!("[ALICE]: Table '{}' created.", table_name);
+                    return Ok(true);
                 }
                 
                 Rule::add_row => {
@@ -240,10 +241,11 @@ impl Database {
                     let row = Row::new(values);
                     self.add_row(table_name, row);
                     println!("[ALICE]: Row added to table '{}'.", table_name);
+                    return Ok(true);
                 }
                 Rule::exit => {
-                    println!("[ALICE]: Goodbye!");
-                    std::process::exit(0);
+                    println!("[ALICE]: Getting back to Database manager!");
+                    return Ok(false);
                 }
 
                 Rule::show_table => {
@@ -253,10 +255,11 @@ impl Database {
                         Some(table) => println!("{:#?}", table.rows),
                         _ => println!("Table doesnt exists!")
                     }
+                    return Ok(true);
                 }
 
-                Rule::debug => { self.print() }
-                Rule::view_transactions => { println!("{:#?}", self.transactions); }
+                Rule::debug => { self.print(); return Ok(true); }
+                Rule::view_transactions => { println!("{:#?}", self.transactions); return Ok(true); }
 
                 Rule::archive_myself => {
                     let mut tokens = inner.into_inner();
@@ -264,6 +267,7 @@ impl Database {
                     let json_string = serde_json::to_string(self)?;
                     let mut file = File::create(file_path)?;
                     file.write_all(json_string.as_bytes())?;
+                    return Ok(true);
                 }
 
                 Rule::load_archive => {
@@ -283,6 +287,7 @@ impl Database {
                         }
                         Err(e) => eprintln!("Error reading archive file: {}", e),
                     }
+                    return Ok(true);
                 }
 
                 Rule::commit => {
@@ -290,8 +295,130 @@ impl Database {
                     let json_string = serde_json::to_string(self)?;
                     let mut file = File::create("alice.dbmscore")?;
                     file.write_all(json_string.as_bytes())?;
+                    return Ok(true);
                 }
 
+                _ => unreachable!("Unexpected rule: {:?}", inner.as_rule()),
+            }
+        }
+        return Ok(true);
+    }
+
+    pub fn shell(&mut self) {
+        let mut rl = match Editor::<(), DefaultHistory>::new() {
+            Ok(editor) => editor,
+            Err(e) => {
+                eprintln!("Error initializing line editor: {}", e);
+                return;
+            }
+        };
+        if let Err(_) = rl.load_history("history.txt") {}
+        loop {
+            let readline = rl.readline("> ");
+            match readline {
+                Ok(line) => {
+                    let line = line.trim();
+                    if line.is_empty() { continue; }
+                    let _ = rl.add_history_entry(line);
+                    let _ = rl.save_history("history.txt");
+                    match self.process_command(line) {
+                        Ok(continue_shell) => {
+                            if !continue_shell {
+                                break;  // exit the database shell
+                            }
+                        }
+                        Err(e) => eprintln!("Error: {}", e),
+                    }
+                }
+                Err(ReadlineError::Interrupted) => {
+                    println!("^C");
+                    continue;
+                }
+                Err(ReadlineError::Eof) => {
+                    println!("Goodbye!");
+                    break;
+                }
+                Err(err) => {
+                    eprintln!("Error reading input: {}", err);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+impl AddAssign<Table> for Database {
+    fn add_assign(&mut self, value: Table) {
+        let tx_uuid = self.start_transaction(TType::CreateTable);
+        self.tables.push(value);
+        self.complete_transaction(tx_uuid).unwrap();
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub enum UserRole {Pawn, Bishop, King}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct User {
+    pub username: String,
+    pub password_hash: String,
+    pub role: UserRole,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct DatabaseManager {
+    pub databases: Vec<Database>,
+    pub users: Vec<User>
+}
+
+impl DatabaseManager {
+    pub fn new() -> Self {
+        let mut databases = Vec::<Database>::new();
+        let mut users = Vec::<User>::new();
+        let root_user = User {
+            username: "root".to_string(),
+            password_hash: "63a9f0ea7bb98050796b649e85481845".to_string(),
+            role: UserRole::King,
+        };
+        let mut system_database = Database::new("sys");
+        users.push(root_user);
+        databases.push(system_database);
+
+        Self {databases, users}
+    }
+
+    pub fn find_database(&mut self, name: &str) -> Option<&mut Database> {
+        self.databases.iter_mut().find(|t| t.name == name)
+    }
+
+    pub fn process_command(&mut self, input: &str) -> io::Result<()> {
+        let command_pairs = CommandsParser::parse(Rule::command, input)
+            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e.to_string()))?;
+
+        for command in command_pairs {
+            let inner = command
+                .into_inner()
+                .next()
+                .expect("command should have inner rule");
+            match inner.as_rule() {
+                Rule::create_database => {
+                    let database_name = inner.into_inner().nth(2).unwrap().as_str();
+                    let mut database = Database::new(database_name);
+                    self.databases.push(database);
+                    println!("[ALICE]: Database '{}' created.", database_name);
+                },
+                Rule::debug => {
+                    println!("{:#?}", self);
+                },
+                Rule::select_database => {
+                    let database_name = inner.into_inner().nth(2).unwrap().as_str();
+                    let mut database = self.find_database(database_name).unwrap();
+                    database.shell();
+                }
+                Rule::exit => {
+                    println!("[ALICE]: Goodbye!");
+                    std::process::exit(0)
+                }
                 _ => unreachable!("Unexpected rule: {:?}", inner.as_rule()),
             }
         }
@@ -308,7 +435,7 @@ impl Database {
         };
 
         if let Err(_) = rl.load_history("history.txt") {
-            todo!();
+            
         }
 
         loop {
@@ -343,23 +470,8 @@ impl Database {
     }
 }
 
-impl AddAssign<Table> for Database {
-    fn add_assign(&mut self, value: Table) {
-        let tx_uuid = self.start_transaction(TType::CreateTable);
-        self.tables.push(value);
-        self.complete_transaction(tx_uuid).unwrap();
-    }
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct DatabaseManager {
-    pub databases: Vec<Database>,
-}
-
 fn main() -> io::Result<()> {
-    println!("{}", WELCOME_MESSAGE);
-    let mut database = Database::new("X Service");
-    database.init();
-    database.shell();
+    let mut dm = DatabaseManager::new();
+    dm.shell();
     Ok(())
 }
